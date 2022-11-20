695863d6582b4f638385f16754bee423d5c72458
mb1945 <118310205+mb1945@users.noreply.github.com>
2022-11-20 17:13:55 +1300

    Update Test.js

--- Test.js
+++ Test.js
@@ -8,34 +8,7 @@ var AndroidCalendar = {
 
         i.extra("title", title);
 
-        if (options !== undefined) {
-
-            if (options.desc !== undefined)
-
-                i.extra("description", options.desc);
-
-            if (options.begin !== undefined)
-
-                i.extraLong("beginTime", options.begin.getTime());
-
-            if (options.end !== undefined)
-
-                i.extraLong("endTime", options.end.getTime());
-
-            if (options.location !== undefined) 
-
-              i.extra("eventLocation", options.location);
-
-            if (options.allDay !== undefined) 
-
-              i.extraBool("allDay", options.allDay);
-
-            if (options.emails != undefined) 
-
-              i.extra("android.intent.extra.EMAIL", options.emails.join());
-
-        }
-
+       
         i.send();
 
     }
